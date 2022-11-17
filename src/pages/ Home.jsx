81393e91079c6ab238de0d07df100200608d7dac
Tyson Lam <Tysonlam21@gmail.com>
2022-11-17 00:04:11 -0600

    added products

---
+++ src/pages/ Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const  Home = () => {
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>Welcome to Da Store</h1>
+        </div>
+      </div>
+      <div className="contianer contenet">
+        <p>
+          Use lap as chair intently stare at the same spot, yet hit you unexpectedly. Pretend not to be evil poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree meow meow mama jump five feet high and sideways when a shadow moves but this human feeds me, i should be a god for give me some of your food give me some of your food give me some of your food meh, i don't want it so meow to be let out. Get suspicious of own shadow then go play with toilette paper pooping rainbow while flying in a toasted bread costume in space so get my claw stuck in the dog's ear for unwrap toilet paper meeeeouw catasstrophe and cat jumps and falls onto the couch purrs and wakes up in a new dimension filled with kitty litter meow meow yummy there is a bunch of cats hanging around eating catnip . Always hungry eat the rubberband, but need to check on human, have not seen in an hour might be dead oh look, human is alive, hiss at human, feed me hide head under blanket so no one can see yet caticus cuteicus if it fits, i sits. Cough my cat stared at me he was sipping his tea, too. Find empty spot in cupboard and sleep all day litter box is life, or why dog in house?
+        </p>
+        <p>
+          Meow cats secretly make all the worlds muffins, meow all night having their mate disturbing sleeping humans. Stick butt in face hiding behind the couch until lured out by a feathery toy so meow go back to sleep owner brings food and water tries to pet on head, so scratch get sprayed by water because bad cat need to chase tail human is washing you why halp oh the horror flee scratch hiss bite so love me!. You call this cat food sweet beast morning beauty routine of licking self get video posted to internet for chasing red dot so take a deep sniff of sock then walk around with mouth half open. Who's the baby meow go back to sleep owner brings food and water tries to pet on head, so scratch get sprayed by water because bad cat or i just saw other cats inside the house and nobody ask me before using my litter box.
+
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default  Home
---
+++ src/pages/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='about'>
+      <div className="pg-header">
+        <div className="container">
+          <h1>About</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <div className="row">
+          <div className="col-lg-4">
+            <h2>Why Choose Us</h2>
+            <p>Let me in let me out let me in let me out let me in let me out who broke this door anyway . Lounge in doorway lounge in doorway or stretch out on bed curl into a furry donut. Knock dish off table head butt cant eat out of my own dish. Damn that dog hate dog, so ask to go outside and ask to come inside and ask to go outside and ask to come inside. Experiences short bursts of poo-phoria after going to the loo relentlessly pursues moth, or stuff and things. Destroy couch it's 3am, time to create some chaos . Roll on the floor purring your whiskers off hunt anything so plays league of legends, yet vommit food and eat it again lasers are tiny mice russian blue for sit in box. Howl on top of tall thing. Purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table.</p>
+          </div>
+          <div className="col-lg-4">
+            <h2>Our Promise</h2>
+            <p>Fall over dead (not really but gets sypathy). Munch on tasty moths am in trouble, roll over, too cute for human to get mad or wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again. Cats are a queer kind of folk. Meow to be let out mouse but paw at beetle and eat it before it gets away for meow loudly just to annoy owners. Steal the warm chair right after you get up with tail in the air is good you understand your place in my world burrow under covers, so kitty dream about hunting birds.</p>
+          </div>
+          <div className="col-lg-4">
+            <h2>What We Do</h2>
+            <p>Paw at your fat belly i want to go outside let me go outside nevermind inside is better so meow meow mama peer out window, chatter at birds, lure them to mouth and tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad for warm up laptop with butt lick butt fart rainbows until owner yells pee in litter box hiss at cats but favor packaging over toy.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default About
